9e28d377b2ba3fa3b6d2131378ea437e3c7bc2fc
saheedsalawu <106780350+saheedsalawu@users.noreply.github.com>
2022-12-22 10:14:40 -0900

    Create relationship_city.py

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Define State model """
+from relationship_state import Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer, ForeignKey
+
+
+class City(Base):
+    """ Define a class State to be linked to db table """
+    __tablename__ = 'cities'
+
+    id = Column(Integer, nullable=False,
+                autoincrement=True, unique=True,
+                primary_key=True)
+
+    name = Column(String(128), nullable=False)
+
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+
+    def __init__(self, **kwargs):
+        """initialize object"""
+        self.__dict__.update(kwargs)
